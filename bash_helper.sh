d65397e771c6ddfe3a8c736c236ba246843741cf
Joshua DeGagne <joshdegagne@gmail.com>
2016-11-23 15:27:37 -0500

    add ghead

--- bash_helper.sh
+++ bash_helper.sh
@@ -3,9 +3,12 @@ source ~/.git-prompt.sh
 PS1="< \u \w$(__git_ps1 ' [%s]') > "
 
 alias ls='ls -GkFlAsh'
-alias tree='tree -FI *.class --prune'
+alias tree='tree -FI *.class --prune' # tree that woks cleanly for clojure projects
 alias ftp='ftp -i'
 alias untargz='tar -zxvf'
 function targz(){
     tar -zcvf $1.tar.gz $1
+}
+function ghead(){ # useful for peaking at csv.gz headers
+    gzip -cd $1 | head
 }
