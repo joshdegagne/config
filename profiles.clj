8b401420950545cca566fe81249a4226ba23f77b
Joshua DeGagne <joshdegagne@gmail.com>
2016-11-23 15:26:59 -0500

    update profiles

--- profiles.clj
+++ profiles.clj
@@ -18,50 +18,56 @@
                                "conj" "[venantius/yagni \"0.1.4\"]"
                                "--" "yagni"]
                   "cljfmt"    ["update-in" ":plugins"
-                               "conj" "[lein-cljfmt \"0.5.3\"]"
+                               "conj" "[lein-cljfmt \"0.5.6\"]"
                                "--" "cljfmt"]
                   ;; automated testing
                   "cloverage" ["update-in" ":plugins"
-                               "conj" "[lein-cloverage \"1.0.6\"]"
+                               "conj" "[lein-cloverage \"1.0.7\"]"
                                "--" "cloverage"]
                   "test-out"  ["update-in" ":plugins"
                                "conj" "[lein-test-out \"0.3.1\"]"
                                "--" "test-out"]
                   ;; documentation
                   "codox"       ["update-in" ":plugins"
-                                 "conj" "[codox \"0.9.5\"]"
+                                 "conj" "[codox \"0.10.0\"]"
                                  "--" "codox"]
                   "clojuredocs" ["update-in" ":plugins"
                                  "conj" "[lein-clojuredocs \"1.0.2\"]"
                                  "--" "clojuredocs"]
                   ;; package management
                   "ancient" ["update-in" ":plugins"
-                             "conj" "[lein-ancient \"0.6.8\"]"
+                             "conj" "[lein-ancient \"0.6.10\"]"
                              "--" "ancient"]
                   ;; application server
                   "immutant" ["update-in" ":plugins"
                               "conj" "[lein-immutant \"2.1.0\"]"
                               "--" "immutant"]}
-        :plugins [[venantius/ultra "0.3.4"]]
-        :ultra {:color-scheme :solarized_dark}}
+        ;; causes: Exception in thread "main" java.lang.RuntimeException:
+        ;; No such var: pretty-repl/pretty-print-stack-trace, compiling:(ultra/stacktrace.clj:11:5)
+        ;; :plugins [[venantius/ultra "0.5.0"]]
+        ;; :ultra {:color-scheme :solarized_dark}
+        }
  :repl {:repl-options {:timeout 12000000}
         :jvm-opts ["-XX:-OmitStackTraceInFastThrow"]
         :plugins [;; ide
-                  [cider/cider-nrepl "0.13.0-SNAPSHOT"]
+                  [cider/cider-nrepl "0.14.0-SNAPSHOT"]
                   [refactor-nrepl "2.3.0-SNAPSHOT"]
                   ;; bytecode inspection
-                  [lein-nodisassemble "0.1.3"]]
+                  ;; [lein-nodisassemble "0.1.3"]
+                  ]
         :dependencies [[org.clojure/tools.nrepl "0.2.12"]
                        [org.clojars.gjahad/debug-repl "0.3.3"]
-                       [org.clojure/tools.trace "0.7.8"]
-                       [org.clojure/tools.namespace "0.2.10"]
+                       [org.clojure/tools.trace "0.7.9"]
+                       [org.clojure/tools.namespace "0.2.11"]
                        ;; benchmarking
-                       [criterium "0.4.3"]
+                       [criterium "0.4.4"]
                        ;; repl debugging
-                       [spyscope "0.1.5"]
-                       [philoskim/debux "0.2.0"]
+                       [spyscope "0.1.6"]
+                       [philoskim/debux "0.2.1"]
                        [dire "0.5.4"]
-                       [com.rpl/specter "0.9.3"]]
+                       [com.rpl/specter "0.13.0"]
+                       ;; visualization
+                       [rhizome "0.2.7"]]
         :injections [(use 'debux.core)
                      (use 'clojure.java.io)
                      (require 'spyscope.core)
