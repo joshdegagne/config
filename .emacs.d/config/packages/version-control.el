288a00539f1f8d48702668d65dc239a2c1156747
Joshua DeGagne <joshdegagne@gmail.com>
2016-11-23 16:36:44 -0500

    use a less buggy thing that is actually provided by magit

--- .emacs.d/config/packages/version-control.el
+++ .emacs.d/config/packages/version-control.el
@@ -5,22 +5,12 @@
 
 (use-package magit :pin melpa-stable :ensure t
   :init (progn (setq magit-last-seen-setup-instructions "1.4.0")
+               (setq magit-display-buffer-function
+                     #'magit-display-buffer-fullframe-status-v1)
                (setq-default magit-save-some-buffers nil
                              magit-process-popup-time 10
                              magit-diff-refine-hunk t
-                             magit-completing-read-function 'magit-ido-completing-read)
-
-               (defadvice magit-status (around magit-fullscreen activate)
-                 (window-configuration-to-register :magit-fullscreen)
-                 ad-do-it
-                 (delete-other-windows))
-
-               (defun magit-quit-session ()
-                 "Restores the previous window configuration and kills the magit buffer"
-                 (interactive)
-                 (kill-buffer)
-                 (when (get-register :magit-fullscreen)
-                   (ignore-errors (jump-to-register :magit-fullscreen)))))
+                             magit-completing-read-function 'magit-ido-completing-read))
   :bind ("C-x m" . magit-status)
   :config (progn
             (defun magit-ignore-whitespace ()
@@ -39,7 +29,6 @@
                   (magit-dont-ignore-whitespace)
                 (magit-ignore-whitespace)))
 
-            (bind-key "q" 'magit-quit-session magit-status-mode-map)
             (bind-key "C-x C-k" 'magit-kill-file-on-line magit-status-mode-map)
             (bind-key "W" 'magit-toggle-whitespace magit-status-mode-map)))
 
