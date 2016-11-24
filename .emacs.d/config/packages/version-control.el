a4cd2781446612f5a6b1dedbf226cdec7edba53c
Joshua DeGagne <joshdegagne@gmail.com>
2016-11-24 10:29:07 -0500

    remove newlines

--- .emacs.d/config/packages/version-control.el
+++ .emacs.d/config/packages/version-control.el
@@ -5,32 +5,25 @@
 
 (use-package magit :ensure t
   :init (progn (setq magit-last-seen-setup-instructions "1.4.0")
-               (setq magit-display-buffer-function
-                     #'magit-display-buffer-fullframe-status-v1)
-               (setq-default magit-save-some-buffers nil
-                             magit-process-popup-time 10
+               (setq magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1)
+               (setq-default magit-process-popup-time 10
                              magit-diff-refine-hunk t
                              magit-completing-read-function 'magit-ido-completing-read))
   :bind ("C-x m" . magit-status)
-  :config (progn
-            (defun magit-ignore-whitespace ()
-              (interactive)
-              (add-to-list 'magit-diff-options "-w")
-              (magit-refresh))
-
-            (defun magit-dont-ignore-whitespace ()
-              (interactive)
-              (setq magit-diff-options (remove "-w" magit-diff-options))
-              (magit-refresh))
-
-            (defun magit-toggle-whitespace ()
-              (interactive)
-              (if (member "-w" magit-diff-options)
-                  (magit-dont-ignore-whitespace)
-                (magit-ignore-whitespace)))
-
-            (bind-key "C-x C-k" 'magit-kill-file-on-line magit-status-mode-map)
-            (bind-key "W" 'magit-toggle-whitespace magit-status-mode-map)))
+  :config (progn (defun magit-ignore-whitespace ()
+                   (interactive)
+                   (add-to-list 'magit-diff-options "-w")
+                   (magit-refresh))
+                 (defun magit-dont-ignore-whitespace ()
+                   (interactive)
+                   (setq magit-diff-options (remove "-w" magit-diff-options))
+                   (magit-refresh))
+                 (defun magit-toggle-whitespace ()
+                   (interactive)
+                   (if (member "-w" magit-diff-options)
+                       (magit-dont-ignore-whitespace)
+                     (magit-ignore-whitespace)))
+                 (bind-key "W" 'magit-toggle-whitespace magit-status-mode-map)))
 
 (defadvice vc-annotate (around fullscreen activate)
   (window-configuration-to-register :vc-annotate-fullscreen)
