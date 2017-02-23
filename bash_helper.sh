5ae620ea128cf141857550da3c2f78bec99ee568
Joshua DeGagne <joshdegagne@gmail.com>
2017-02-23 11:03:30 -0500

    bash changes

--- bash_helper.sh
+++ bash_helper.sh
@@ -1,16 +1,23 @@
 
 source ~/.git-prompt.sh
-PS1="< \u \w$(__git_ps1 ' [%s]') > "
+PS1="< \u \w\$(__git_ps1 ' [%s]') > "
 
-alias ls='ls -GkFlAsh'
+alias rebash='source ~/.bash_profile'
+
+alias ls='ls -lhGAF'
 alias tree='tree -FI *.class --prune' # tree that woks cleanly for clojure projects
+
 alias ftp='ftp -i'
+
 alias untargz='tar -zxvf'
 function targz(){
     tar -zcvf $1.tar.gz $1
 }
+
 function ghead(){ # useful for peaking at csv.gz headers
     gzip -cd $1 | head
 }
+
+eval "$(thefuck --alias)"
 
 alias q32='QHOME=~/q rlwrap -r ~/q/l32/q'
