4c764ad7c381f0075b63083c7856c6b92f8ca9a2
Joshua DeGagne <joshdegagne@gmail.com>
2017-09-18 16:26:25 -0400

    fix json indenting

--- emacs.d/config/languages/javascript.el
+++ emacs.d/config/languages/javascript.el
@@ -1,15 +1,7 @@
-(use-package js2-mode :ensure t
-  :mode "\\.js$"
-  :config (progn
-            (add-hook 'js2-post-parse-callbacks
-                      ;; adds symbols included by google closure to js2-additional-externs
-                      (lambda ()
-                        (let ((buf (buffer-string))
-                              (index 0))
-                          (while (string-match "\\(goog\\.require\\|goog\\.provide\\)('\\([^'.]*\\)" buf index)
-                            (setq index (+ 1 (match-end 0)))
-                            (add-to-list 'js2-additional-externs (match-string 2 buf))))))
-            (diminish-major-mode 'js2-mode "J")))
+(add-hook 'json-mode-hook
+          (lambda ()
+            (make-local-variable 'js-indent-level)
+            (setq js-indent-level 2)))
 
 (use-package coffee-mode :ensure t)
 (custom-set-variables '(coffee-tab-width 2))
