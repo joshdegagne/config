6dbda7c0d783f0637d8d48c14bcb602c4962e457
Joshua DeGagné <joshdegagne@gmail.com>
2017-11-18 23:22:46 -0500

    remove powerline

--- emacs.d/config/packages/ui.el
+++ emacs.d/config/packages/ui.el
@@ -12,69 +12,6 @@
 (use-package rainbow-mode :ensure t
   :diminish rainbow-mode)
 
-(use-package string-utils :ensure t)
-(use-package projectile)
-
-(use-package powerline :ensure t
-  :init (setq powerline-default-separator 'wave)
-  :config (progn
-            (require 'powerline)
-            (add-hook 'desktop-after-read-hook 'powerline-reset)
-            (defface modes-ml-face '((t (:background "#002b36" :inherit mode-line)))
-              "Powerline face for modes section of the mode-line"
-              :group 'powerline)
-            (defface file-ml-face '((t (:background "#586e75" :inherit mode-line)))
-              "Powerline face for file and branch section of the mode-line"
-              :group 'powerline)
-            (defface line-ml-face '((t (:background "#93a1a1" :inherit mode-line)))
-              "Powerline face for line number section of the mode-line"
-              :group 'powerline)
-            (defface pos-ml-face '((t (:background "#586e75" :inherit mode-line)))
-              "Powerline face for file position section of the mode-line"
-              :group 'powerline)
-            (defface ml-fill-face '((t (:background "#93a1a1" :inherit mode-line)))
-              "Powerline face used to fill the unused portion of the mode-line"
-              :group 'powerline)
-            (setq-default mode-line-format
-                          '("%e"
-                            (:eval
-                             (let* ((file-name (buffer-file-name (current-buffer)))
-                                    (active (powerline-selected-window-active))
-                                    (separator-left (intern (format "powerline-%s-%s"
-                                                                    (powerline-current-separator)
-                                                                    (car powerline-default-separator-dir))))
-                                    (separator-right (intern (format "powerline-%s-%s"
-                                                                     (powerline-current-separator)
-                                                                     (cdr powerline-default-separator-dir))))
-                                    (lhs (list (powerline-major-mode 'modes-ml-face 'l)
-                                               (powerline-process 'modes-ml-face 'l)
-                                               (powerline-minor-modes 'modes-ml-face 'l)
-                                               (powerline-raw " " 'modes-ml-face)
-                                               (funcall separator-left 'modes-ml-face 'file-ml-face)
-
-                                               (powerline-raw "[" 'file-ml-face)
-                                               (powerline-raw (projectile-project-name) 'file-ml-face)
-                                               (powerline-raw "] %b %*" 'file-ml-face)
-                                               (powerline-raw (concat " "
-                                                                      (when (and file-name vc-mode)
-                                                                        (concat "(" (-> file-name
-                                                                                        vc-git--symbolic-ref
-                                                                                        (string-utils-truncate-to 40))
-                                                                                ")")))
-                                                              'file-ml-face 'r)
-                                               (funcall separator-left 'file-ml-face 'ml-fill-face)))
-
-                                    (rhs (list (powerline-raw global-mode-string 'ml-fill-face 'r)
-                                               (funcall separator-right 'ml-fill-face 'pos-ml-face)
-                                               (powerline-raw "%p " 'pos-ml-face 'l)
-                                               (funcall separator-right 'pos-ml-face 'line-ml-face)
-
-                                               (powerline-raw " %4l " 'line-ml-face 'r))))
-
-                               (concat (powerline-render lhs)
-                                       (powerline-fill 'ml-fill-face (powerline-width rhs))
-                                       (powerline-render rhs))))))))
-
 (use-package popwin :ensure t
   :init (setq display-buffer-function 'popwin:display-buffer
               popwin:special-display-config
@@ -83,8 +20,6 @@
                 ("*compilation*" :noselect t)
                 ("*Messages*" :height 30)
                 ("*Occur*" :noselect t)
-                ("\\*Slime Description.*" :noselect t :regexp t :height 30)
-                ("\\*Slime Inspector.*" :regexp t :height 30)
                 ("*sldb.*":regexp t :height 30)
                 ("*magit-commit*" :noselect t :height 40 :width 80)
                 ("*magit-diff*" :noselect t :height 40 :width 80)
@@ -99,7 +34,6 @@
                 ("*Compile-Log*" :height 30)
                 (" *auto-async-byte-compile*" :height 14 :position bottom)
                 ("*VC-log*" :height 10 :position bottom)
-
                 ("*nREPL doc*" :height 30 :position bottom)
                 ("*nREPL error*" :height 30 :position bottom)
                 ("*nREPL inspect*" :height 20 :position bottom)
