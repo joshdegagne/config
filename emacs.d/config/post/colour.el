d397ecac99560c86621325667487ef644264d38e
Joshua DeGagne <joshdegagne@gmail.com>
2017-12-01 10:19:14 -0500

    fiddle with base16 themes

--- emacs.d/config/post/colour.el
+++ emacs.d/config/post/colour.el
@@ -1,5 +1,12 @@
+;; remember to delete .emacs.desktop if colours don't change
 (use-package color-theme-solarized :ensure t)
+;;(load-theme 'solarized t)
+
 (use-package base16-theme :ensure t)
-;; remember to delete emacs-custom.el if colours don't change
-;; (load-theme 'solarized t)
-(load-theme 'base16-chalk t)
+;;(load-theme 'base16-atelier-sulphurpool-light t)
+;;(load-theme 'base16-cupcake t)
+;;(load-theme 'base16-gruvbox-dark-hard t)
+(load-theme 'base16-nord t)
+;;(load-theme 'base16-paraiso t)
+;;(load-theme 'base16-solarized-dark t)
+;;(load-theme 'base16-solarized-light t)
