ddd2d7feec88be147d832b9c5519877376fc22bc
Joshua DeGagné <joshdegagne@gmail.com>
2017-11-30 19:51:43 -0500

    fa-lala-lala-lala-lala

--- bash_helper.sh
+++ bash_helper.sh
@@ -7,11 +7,13 @@ PS1="[\u \w\$(__git_ps1 ' <%s>')]$ "
 alias rebash='source ~/.bash_profile'
 
 alias ls='ls -lhGAF'
+
 alias cljtree='tree -FI *.class --prune' # tree that woks cleanly for clojure projects
 
 alias ftp='ftp -i' # no interactive prompt
 
 alias untargz='tar -zxvf'
+
 function targz(){
     tar -zcvf $1.tar.gz $1
 }
@@ -23,3 +25,13 @@ function ghead(){ # useful for peaking at csv.gz headers
 eval "$(thefuck --alias)"
 
 alias q32='QHOME=~/q rlwrap -r ~/q/l32/q'
+
+## erlang
+export PATH=~/.cache/rebar3/bin:$PATH
+
+## rust
+export RUST_SRC_STABLE=/user/local/src/rust/stable/src
+export RUST_SRC_BETA=/user/local/src/rust/beta/src
+export RUST_SRC_NIGHTLY=/user/local/src/rust/nightly/src
+export RUST_SRC_PATH=$RUST_SRC_BETA
+export CARGO_HOME=~/.cargo
