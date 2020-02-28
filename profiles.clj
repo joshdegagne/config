29d279947817e8940fdc5dab5e0839483b1ecf88
joshua degagné <joshdegagne@gmail.com>
2020-02-28 16:55:16 -0500

    update and simplify profiles.clj

--- profiles.clj
+++ profiles.clj
@@ -1,63 +1,28 @@
-{:user {:aliases {;; "repl" ["do" ["clean"] ["repl"]] ;; comment out to:   lein repl :connect
-                  "dumbrepl" ["with-profile" "repl" ["trampoline" "run" "-m" "clojure.main/main"]]
+{:user {:aliases {"dumbrepl" ["with-profile" "repl" ["trampoline" "run" "-m" "clojure.main/main"]]
+                  ;; formatting
+                  "cljfmt" ["update-in" ":plugins" "conj" "[lein-cljfmt \"0.6.4\"]" "--" "cljfmt"]
                   ;; linters
+                  "bikeshed"  ["update-in" ":plugins" "conj" "[lein-bikeshed \"0.5.2\"]" "--" "bikeshed"]
+                  "eastwood"  ["update-in" ":plugins" "conj" "[jonase/eastwood \"0.3.5\"]" "--" "eastwood"]
                   "kibit"     ["update-in" ":plugins" "conj" "[lein-kibit \"0.1.6\" :exclusions [org.clojure/clojure]]" "--" "kibit"]
-                  "eastwood"  ["update-in" ":plugins" "conj" "[jonase/eastwood \"0.2.5\"]" "--" "eastwood"]
-                  "slamhound" ["update-in" ":dependencies" "conj" "[slamhound \"1.5.5\"]" "--" ["run" "-m" "slam.hound"]]
-                  "bikeshed"  ["update-in" ":plugins" "conj" "[lein-bikeshed \"0.4.0\"]" "--" "bikeshed"]
-                  "cljfmt"    ["update-in" ":plugins" "conj" "[lein-cljfmt \"0.5.6\"]" "--" "cljfmt"]
-                  "yagni"     ["update-in" ":plugins" "conj" "[venantius/yagni \"0.1.4\"]" "--"
+                  "yagni"     ["update-in" ":plugins" "conj" "[venantius/yagni \"0.1.7\"]" "--"
                                "update-in" ":source-paths " "conj" "\"test\"" "--"
                                "yagni"]
-                  ;; automated testing
-                  "cloverage" ["update-in" ":plugins" "conj" "[lein-cloverage \"1.0.9\"]" "--" "cloverage"]
-                  "test-out"  ["update-in" ":plugins" "conj" "[lein-test-out \"0.3.1\"]" "--" "test-out"]
+                  ;; testing
+                  "cloverage" ["update-in" ":plugins" "conj" "[lein-cloverage \"1.1.1\"]" "--" "cloverage"]
                   ;; documentation
-                  "codox"       ["update-in" ":plugins" "conj" "[codox \"0.10.1\"]" "--" "codox"]
+                  "codox"       ["update-in" ":plugins" "conj" "[codox \"0.10.6\"]" "--" "codox"]
                   "clojuredocs" ["update-in" ":plugins" "conj" "[lein-clojuredocs \"1.0.2\"]" "--" "clojuredocs"]
-                  ;; package management
-                  "ancient" ["update-in" ":plugins" "conj" "[lein-ancient \"0.6.10\" :exclusions [org.clojure/clojure]]" "--" "ancient"]
-                  ;; application server
-                  "immutant" ["update-in" ":plugins" "conj" "[lein-immutant \"2.1.0\"]" "--" "immutant"]}
-        :plugins [[venantius/ultra "0.5.0"]]
-        :ultra {:color-scheme :solarized_dark}}
+                  ;; dependency management
+                  "ancient" ["update-in" ":plugins" "conj" "[lein-ancient \"0.6.15\" :exclusions [org.clojure/clojure]]" "--" "ancient"]}}
  :repl {:repl-options {:timeout 12000000}
         :jvm-opts ["-XX:-OmitStackTraceInFastThrow"]
-        :plugins [[cider/cider-nrepl "0.16.0"]
-                  [refactor-nrepl "2.3.1" :exclusions [alembic]]
-                  ;; bytecode inspection
-                  [lein-nodisassemble "0.1.3"]]
-        :dependencies [[org.clojure/tools.nrepl "0.2.13"]
-                       [org.clojars.gjahad/debug-repl "0.3.3"]
-                       [difform "1.1.2"]
-                       [clojure-emacs/alembic "0.3.3"]
-                       [org.clojure/data.avl "0.0.17"]
-                       [im.chit/lucid.core.inject "1.2.0"]
-                       [im.chit/lucid.mind "1.2.0"]
-                       [io.aviso/pretty "0.1.31"]
-                       ;; debugging
+        :plugins [[cider/cider-nrepl "0.21.1"]
+                  [refactor-nrepl "2.4.0"]]
+        :dependencies [;; debugging
                        [jsofra/data-scope "0.1.2"]
                        [philoskim/debux "0.2.1"]
                        [org.clojure/tools.trace "0.7.9"]
                        [org.clojure/tools.namespace "0.2.11"]
-                       ;; benchmarking
-                       [criterium "0.4.4"] ; clj only
-                       [com.taoensso/tufte "1.1.1"] ; clj/s
                        ;; visualization
-                       [rhizome "0.2.7"]]
-        :injections [(require '[clojure.java.io :as jio]
-                              '[debux.core :as dx]
-                              '[lucid.core.inject :as inject]
-                              'data-scope.charts
-                              'data-scope.graphs
-                              'data-scope.inspect
-                              'data-scope.pprint)
-                     (inject/in [lucid.core.inject :refer [inject [in inject-in]]]
-                                [clojure.pprint pprint]
-                                [clojure.java.shell sh]
-                                [debux.core dbg]
-                                ;;[alembic.still [distill pull] lein [load-project pull-project]]
-                                [clojure.tools.namespace.repl refresh]
-                                [clojure.repl doc source]
-                                clojure.core
-                                [lucid.mind .& .> .? .* .% .%> .>var .>ns])]}}
+                       [rhizome "0.2.7"]]}}
