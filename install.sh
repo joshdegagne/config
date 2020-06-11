2ba970819ae99b2ac82bb93d7423933408b09879
joshua degagné <joshdegagne@gmail.com>
2020-06-11 16:57:33 -0400

    keep my aspell dictionary

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ rm -rf ~/.emacs.d
 rm -f ~/.emacs
 # install emacs configuration
 ln -sf `pwd`/emacs.d ~/.emacs.d
+# personal dictionary!
+ln -sd `pwd`/aspell.en.pws ~/.aspell.en.pws
 # install clojure tooling
 ln -sf `pwd`/profiles.clj ~/.lein/profiles.clj
 # install shell configuration
