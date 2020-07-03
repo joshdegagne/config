ee49173ea2ca10bbfe1a9d7b0f6efd4f1a7c7343
joshua degagné <joshdegagne@gmail.com>
2020-07-03 09:57:48 -0400

    fish highlighting wasn't using solarized

--- fish/config.fish
+++ fish/config.fish
@@ -139,6 +139,35 @@ function current_git_branch
   git branch 2> /dev/null | grep "^\*" | cut -d \  -f 2
 end
 
+# FISH HIGHLIGHTING COLOURS --------------------------------------------------------------
+
+## custom solarized light
+set -U fish_color_normal normal
+set -U fish_color_command 268bd2
+set -U fish_color_quote 2aa198
+set -U fish_color_end 6c71c4
+set -U fish_color_redirection b58900
+set -U fish_color_comment 93a1a1
+set -U fish_color_error dc322f
+set -U fish_color_search_match bryellow --background=white
+set -U fish_color_history_current --bold
+set -U fish_color_operator 00a6b2
+set -U fish_color_escape 00a6b2
+set -U fish_color_cwd green
+set -U fish_color_cwd_root red
+set -U fish_color_valid_path --underline
+set -U fish_color_autosuggestion d33682
+set -U fish_color_user brgreen
+set -U fish_color_host normal
+set -U fish_color_cancel -r
+set -U fish_pager_color_completion green
+set -U fish_pager_color_description B3A06D
+set -U fish_pager_color_prefix cyan --underline
+set -U fish_pager_color_progress brwhite --background=cyan
+set -U fish_color_match --background=brblue
+set -U fish_color_param 657b83
+set -U fish_color_selection white --bold --background=brblack
+
 # PATH -----------------------------------------------------------------------------------
 
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
