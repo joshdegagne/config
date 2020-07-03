c3851f9e7477c10e1beeae8bcbc0f5e075349226
joshua degagné <joshdegagne@gmail.com>
2020-07-03 10:32:24 -0400

    make it more like how emacs highlights fish code

--- fish/config.fish
+++ fish/config.fish
@@ -143,12 +143,12 @@ end
 
 ## custom solarized light
 set -U fish_color_normal normal
-set -U fish_color_command 268bd2
-set -U fish_color_quote 2aa198
-set -U fish_color_end 6c71c4
+set -U fish_color_command 859900
 set -U fish_color_redirection b58900
+set -U fish_color_quote 2aa198
+set -U fish_color_param 268bd2
 set -U fish_color_comment 93a1a1
-set -U fish_color_error dc322f
+set -U fish_color_error d33682
 set -U fish_color_search_match bryellow --background=white
 set -U fish_color_history_current --bold
 set -U fish_color_operator 00a6b2
@@ -156,7 +156,7 @@ set -U fish_color_escape 00a6b2
 set -U fish_color_cwd green
 set -U fish_color_cwd_root red
 set -U fish_color_valid_path --underline
-set -U fish_color_autosuggestion d33682
+set -U fish_color_autosuggestion 657b83
 set -U fish_color_user brgreen
 set -U fish_color_host normal
 set -U fish_color_cancel -r
@@ -165,7 +165,7 @@ set -U fish_pager_color_description B3A06D
 set -U fish_pager_color_prefix cyan --underline
 set -U fish_pager_color_progress brwhite --background=cyan
 set -U fish_color_match --background=brblue
-set -U fish_color_param 657b83
+set -U fish_color_end 6c71c4
 set -U fish_color_selection white --bold --background=brblack
 
 # PATH -----------------------------------------------------------------------------------
