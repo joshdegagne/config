e5fa137eb7aa6ba037bf401a6b22eafd5272d484
joshua degagné <joshdegagne@gmail.com>
2020-07-31 14:21:37 -0400

    eh, don't need to customize that much of the prompt pwd

--- fish/config.fish
+++ fish/config.fish
@@ -9,8 +9,6 @@ set prompt_pwd_colour brred
 set prompt_symbol_colour normal
 set colour_order magenta brmagenta blue cyan green yellow
 set delim_width (string length $delim)
-set fish_prompt_pwd_raw_levels 2
-set fish_prompt_pwd_dir_length 2
 
 function fish_prompt
   # extra space is relaxing
@@ -33,52 +31,6 @@ function fish_prompt
          (set_color $prompt_pwd_colour; prompt_pwd) \
          (set_color $prompt_symbol_colour; echo $prompt_symbol)
 end
-
-function prompt_pwd --description 'Print the current working directory, shortened to fit the prompt'
-  set -l options 'h/help'
-  argparse -n prompt_pwd --max-args=0 $options -- $argv
-  or return
-
-  if set -q _flag_help
-    __fish_print_help prompt_pwd
-    return 0
-  end
-
-  # This allows overriding fish_prompt_pwd_dir_length and fish_prompt_pwd_raw_levels from the outside (global or universal) without leaking them
-  set -q fish_prompt_pwd_dir_length
-  or set -l fish_prompt_pwd_dir_length 1
-  set -q fish_prompt_pwd_raw_levels
-  or set -l fish_prompt_pwd_raw_levels 1
-
-  # Replace $HOME with "~"
-  set realhome ~
-  set -l tmp (string replace -r '^'"$realhome"'($|/)' '~$1' $PWD)
-
-  if [ $fish_prompt_pwd_dir_length -eq 0 ]
-    echo $tmp
-  else
-    # Shorten to at most $fish_prompt_pwd_dir_length characters per directory
-    # and don't shorten at most $fish_prompt_pwd_raw_levels directories at the end
-    set p (string split \/ $tmp)
-    set n (count $p)
-    for i in (seq $n)
-      if test $i -le (math $n - $fish_prompt_pwd_raw_levels)
-        printf "%s/" (string sub -l $fish_prompt_pwd_dir_length $p[$i])
-      else if test $i -eq $n
-        printf "%s" $p[$i]
-      else
-        printf "%s/" $p[$i]
-      end
-    end
-  end
-end
-
-##### WIP
-function __fish_winch_handler --on-signal SIGWINCH
-__fish_cancel_commandline
-commandline -C (count ) -f repaint >/dev/null 2>/dev/null
-end
-##### END WIP
 
 function fish_title
   set -l command (echo $_)
