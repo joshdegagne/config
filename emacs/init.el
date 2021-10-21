6dfab76e203590ac881026997ea470fc1ba45052
josh.degagne <joshdegagne@gmail.com>
2021-10-21 14:56:32 -0400

    command map

--- emacs/init.el
+++ emacs/init.el
@@ -223,8 +223,7 @@
 (use-package projectile :ensure t
   :init (projectile-mode +1)
   :bind (:map projectile-mode-map
-              ("s-p" . projectile-command-map)
-              ("C-c p" . projectile-command-map)))
+              ("C-x p" . projectile-command-map)))
 
 (use-package projectile-ripgrep :ensure t :after projectile)
 
