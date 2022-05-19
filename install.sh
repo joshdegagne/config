f7272003f1a615574789076a9486355169a26bc9
Joshua DeGagné <joshdegagne@gmail.com>
2022-05-19 19:48:25 -0400

    add starship.toml for fish-style path truncating

--- install.sh
+++ install.sh
@@ -15,8 +15,13 @@ ln -sf `pwd`/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
 # zsh configuration
 ln -sf `pwd`/.zshrc ~/.zshrc
 
+# starship configuration
+ln -sf `pwd`/starship.toml ~/.config/starship.toml
+
 # clojure tooling
 ln -sf `pwd`/clojure/leiningen/profiles.clj ~/.lein/profiles.clj
+
+# need to figure our clojure tooling in a deps.edn environment.
 
 # elixir tooling
 ln -sf `pwd`/elixir/i.ex ~/.iex.exs
